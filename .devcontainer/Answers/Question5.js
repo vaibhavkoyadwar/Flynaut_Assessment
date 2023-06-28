c01ee60fadfad6eba7ec2c2ccb87b4d040fb022e
vaibhavkoyadwar <119652236+vaibhavkoyadwar@users.noreply.github.com>
2023-06-28 09:29:14 +0530

    Question5.js

--- .devcontainer/Answers/Question5.js
+++ .devcontainer/Answers/Question5.js
@@ -1,4 +1,4 @@
 function average(a, b) {
     return a + b / 2;
     }
-    console.log(average(2, 1));      //1.5
+    console.log(average(2, 1));      //Ans=1.5  ,function will called and the values will be send to it  which will result in the output as 1.5
