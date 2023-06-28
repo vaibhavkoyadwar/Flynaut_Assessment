38d30ce0a496faa42893d7d4e862a033d51431b4
vaibhavkoyadwar <119652236+vaibhavkoyadwar@users.noreply.github.com>
2023-06-28 09:18:40 +0530

    Question3.js

--- .devcontainer/Answers/Question3.js
+++ .devcontainer/Answers/Question3.js
@@ -1,7 +1,7 @@
 let obj1 = { "greeting": "hello" };
 let obj2 = obj1;
-obj1["greeting"] = "Bye";
+obj1["greeting"] = "Bye";  //from this line bye will be the value for "greeting
 
 
 console.log(obj1);    //{ greeting: 'Bye' }
-console.log(obj2);    //{ greeting: 'Bye' }
+console.log(obj2);    //{ greeting: 'Bye' }  // as this both obj1  and obj2 are sharing the same refrence variable the output will be same
